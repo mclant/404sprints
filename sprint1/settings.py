9aab993be7fc52d9b019fcc685501f7c64cc9159
Matt Lant <matthewclant@gmail.com>
2019-02-13 17:53:15 -0700

    migration issues fixed

--- sprint1/settings.py
+++ sprint1/settings.py
@@ -101,8 +101,12 @@ AUTH_USER_MODEL = 'account.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sprint1db',
+        'USER': 'Matt',
+        'PASSWORD': '37Sibh7h!',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
