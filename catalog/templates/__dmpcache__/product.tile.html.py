22a3b7abaac3198ebc105f889fe4d8b85ec18907
Matt Lant <matthewclant@gmail.com>
2019-03-13 11:21:39 -0600

    i hate my life

--- catalog/templates/__dmpcache__/product.tile.html.py
+++ catalog/templates/__dmpcache__/product.tile.html.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1552494292.855853
+_modified_time = 1552495999.819117
 _enable_loop = True
 _template_filename = '/Users/Matt/Documents/School/IS 413/sprint1/catalog/templates/product.tile.html'
 _template_uri = 'product.tile.html'
@@ -25,15 +25,15 @@ def _mako_generate_namespaces(context):
     pass
 def _mako_inherit(template, context):
     _mako_generate_namespaces(context)
-    return runtime._inherit_from(context, '/catalog/templates/base_ajax.htm', _template_uri)
+    return runtime._inherit_from(context, '/homepage/templates/base_ajax.htm', _template_uri)
 def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         def content():
             return render_content(context._locals(__M_locals))
-        self = context.get('self', UNDEFINED)
         product = context.get('product', UNDEFINED)
+        self = context.get('self', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
@@ -50,8 +50,8 @@ def render_content(context,**pageargs):
     try:
         def content():
             return render_content(context)
-        self = context.get('self', UNDEFINED)
         product = context.get('product', UNDEFINED)
+        self = context.get('self', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n<a href="/catalog/product/')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)(product.id))
